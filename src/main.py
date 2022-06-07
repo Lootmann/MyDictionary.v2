c270e47107d06cbcff2c34818db7eb474345d9d0
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-07 17:42:36 +0900

    main: add fetch, cache, show word info flow

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from logging import config
 
 import jstyleson
 
+from src.cache import create_cache_dir
+
 
 def init():
     # logger settings
@@ -15,7 +17,32 @@ def init():
 
 
 def main():
-    pass
+    # 1. create cache_dir
+    create_cache_dir()
+
+    # 2. get user inputs called 'word_name'
+    #       if fail to get user inputs, END :^)
+
+    # 3. cache flow
+
+    # 3.1 try to find 'word_name'.cache,
+    #       if cache file is found, show this cache END :^)
+
+    # 3.2 while not found 'word_name'.cache,
+    #       create 'word_name'.cache
+
+    # 4. create cache flow
+
+    # 4.1 get fetch data from WEBLIO API
+    #       fetch_data('word_name')
+
+    # 4.1.1 if fail to fetch, END :^)
+    # 4.1.2 if success to fetch, parse fetch data (HTML data)
+
+    # 4.2 success to parse fetch data,
+    #       create 'word_name'.cache using parsed HTML
+
+    # 4.3 show this cache END :^)
 
 
 if __name__ == "__main__":
