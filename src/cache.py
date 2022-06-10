2a9f652aed15671e238417a66a2ebb6172e42818
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-10 17:45:05 +0900

    cache: add create/load cache method, and docstring

--- src/cache.py
+++ src/cache.py
@@ -1,4 +1,17 @@
-# src/cache.py
+""" src/cache.py
+A class to handle 'cache' files.
+
+The meaning of 'handle' is as follows,
+    save cache as json file
+    load cache as dict from cache file
+
+A cache file has these rules
+    1. A cache file named 'word_name'.cache
+    2. 'word_name' has some spaces like
+        'endeavour', 'pull off', 'look forward to', ...
+    3. A cache file is an ordinary json file.
+"""
+import json
 from pathlib import Path
 
 
@@ -8,19 +21,27 @@ class Cache:
     @classmethod
     def cache_dirpath(cls) -> Path:
         """
-        get cache_dirpath
-        :return: Path(CACHE_DIR_PATH)
+        get cache_dirpath. cache_dirpath is type of Pathlib
+
+        :return: /home/user/.cache/mydict/ with Pathlib
         """
         return Path(cls.CACHE_DIR_PATH).expanduser()
 
     @classmethod
     def cache_path(cls, word_name: str) -> Path:
+        """
+        get cache file path.
+
+        :param word_name:
+        :return: /home/user/.cache/mydict/bruh.cache with Pathlib
+        """
         return cls.cache_dirpath() / f"{word_name}.cache"
 
     @classmethod
     def create_cache_dir(cls) -> None:
         """
         create cache_dir if CACHE_DIR_PATH is not found.
+
         :return: None
         """
         dir_path = cls.cache_dirpath()
@@ -31,6 +52,7 @@ class Cache:
     def has_cache(cls, word_name: str) -> bool:
         """
         has_cache finds 'word_name'.cache
+
         :param word_name: str
         :return: return True if word_name.cache has found
             return False if not
@@ -39,5 +61,26 @@ class Cache:
 
     @classmethod
     def create_cache(cls, word_name: str, word_info: dict) -> None:
-        dir_path = cls.cache_dirpath()
-        return
+        """
+        create a 'word_name'.cache file.
+
+        :param word_name: str
+        :param word_info: dict
+        """
+        path = cls.cache_path(word_name)
+
+        with path.open("w", encoding="utf-8") as f:
+            json.dump(word_info, f)
+
+    @classmethod
+    def load_cache(cls, word_name: str) -> dict:
+        """
+        Load 'word_name'.cache.
+
+        :param word_name:
+        :return:
+        """
+        path = cls.cache_path(word_name)
+
+        with path.open("r", encoding="utf-8") as f:
+            return json.load(f)
