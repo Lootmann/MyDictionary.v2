8ecb4975d8b90ae2de4c1f601dd081b91a1d4a64
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-10 15:14:37 +0900

    cache: make cache features classnize

--- src/cache.py
+++ src/cache.py
@@ -1,26 +1,43 @@
 # src/cache.py
 from pathlib import Path
 
-CACHE_DIR_PATH = "~/.cache/mydict/"
 
+class Cache:
+    CACHE_DIR_PATH = "~/.cache/mydict/"
 
-def create_cache_dir() -> None:
-    """
-    create cache_dir if CACHE_DIR_PATH is not found.
-    :return: None
-    """
-    dir_path = Path(CACHE_DIR_PATH).expanduser()
-    if dir_path.exists():
-        dir_path.mkdir()
+    @classmethod
+    def cache_dirpath(cls) -> Path:
+        """
+        get cache_dirpath
+        :return: Path(CACHE_DIR_PATH)
+        """
+        return Path(cls.CACHE_DIR_PATH).expanduser()
 
+    @classmethod
+    def cache_path(cls, word_name: str) -> Path:
+        return cls.cache_dirpath() / f"{word_name}.cache"
 
-def has_cache(word_name: str) -> bool:
-    """
-    has_cache finds 'word_name'.cache
-    :param word_name: str
-    :return: return True if word_name.cache has found
-        return False if not
-    """
-    cache_path = Path(CACHE_DIR_PATH) / f"{word_name}.cache"
-    abs_cache_path = cache_path.expanduser()
-    return abs_cache_path.exists()
+    @classmethod
+    def create_cache_dir(cls) -> None:
+        """
+        create cache_dir if CACHE_DIR_PATH is not found.
+        :return: None
+        """
+        dir_path = cls.cache_dirpath()
+        if not dir_path.exists():
+            dir_path.mkdir()
+
+    @classmethod
+    def has_cache(cls, word_name: str) -> bool:
+        """
+        has_cache finds 'word_name'.cache
+        :param word_name: str
+        :return: return True if word_name.cache has found
+            return False if not
+        """
+        return cls.cache_path(word_name).exists()
+
+    @classmethod
+    def create_cache(cls, word_name: str, word_info: dict) -> None:
+        dir_path = cls.cache_dirpath()
+        return
