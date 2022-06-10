b527cbd0a283ee2a572a6a9cc1753be23fd3153c
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-10 14:15:05 +0900

    cli: parse user inputs, and convert 'word_name'

---
+++ src/cli.py
@@ -0,0 +1,27 @@
+# src/cli.py
+import argparse
+from typing import List
+
+
+def user_input(argv: List[str]) -> str:
+    """
+    Ger user_input
+    :param: None
+    :return: str - word_name
+    """
+    # when no arguments, raise ValueError
+    if len(argv) == 1:
+        raise ValueError("need arguments")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "word",
+        nargs="+",
+        type=str,
+        help="input english word that you want to know the definition",
+        default="noargs",
+    )
+
+    # get command-line args from sys.argv automatically
+    args = parser.parse_args(argv)
+    return " ".join(args.word[1:])
