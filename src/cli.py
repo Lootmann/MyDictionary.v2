c0656c500139e0355a6bd13d6fb980b40d4e07a6
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-10 18:40:42 +0900

    cli: word_info print info prettier

--- src/cli.py
+++ src/cli.py
@@ -24,3 +24,7 @@ def user_input(argv: List[str]) -> str:
     # get command-line args from sys.argv automatically
     args = parser.parse_args(argv)
     return " ".join(args.word[1:])
+
+
+def prettify(word_info: dict) -> None:
+    print(word_info)
