fb5260793437a7f43a60a48e912b2e54d34e44d4
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-14 21:17:25 +0900

    parse: impl Parse.parse when fail to find 'word_name' from Weblio

--- src/parse.py
+++ src/parse.py
@@ -8,12 +8,16 @@ class Parse:
     """
 
     @classmethod
-    def parse(cls, fetch_data_text: str) -> dict:
+    def parse(cls, word_name: str, fetch_data_text: str) -> dict:
         """
         Parse WEBLIO HTML to dict
         :return: dict - word info dict
         """
         soup = BeautifulSoup(fetch_data_text, "lxml")
+
+        # when 'word_name' is not found, this article has no 'kijiWrp' class
+        if soup.find("table", id="anoOnnanoko"):
+            return {f"{word_name}": "not found", "type_of_speech": []}
 
         # main kiji
         main_block = soup.find("kijiWrp")
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,73 +1,90 @@
 # tests/test_parse.py
 from pathlib import Path
 
+import pytest
+
+from src.parse import Parse
+
 
 def get_path(filename: str) -> Path:
     return Path(f"./htmls/{filename}.html")
 
 
-def get_text(path: Path) -> str:
+def get_html(path: Path) -> str:
     return path.read_text()
-
-
-class TestParseNoun:
-    def setup_method(self):
-        path = get_path("dictionary")
-        self.parsed_data = get_text(path)
-
-    def test_get_part_of_speech(self):
-        pass
-
-
-class TestParseVerb:
-    def setup_method(self):
-        path = get_path("learn")
-        self.parsed_data = get_text(path)
-
-    def test_get_part_of_speech(self):
-        pass
-
-
-class TestParseAdjective:
-    def setup_method(self):
-        path = get_path("tremendous")
-        self.parsed_data = get_text(path)
-
-    def test_get_part_of_speech(self):
-        pass
-
-
-class TestParsePreposition:
-    def setup_method(self):
-        path = get_path("into")
-        self.parsed_data = get_text(path)
-
-    def test_get_part_of_speech(self):
-        pass
-
-
-class TestParseConjunction:
-    def setup_method(self):
-        path = get_path("although")
-        self.parsed_data = get_text(path)
-
-    def test_get_part_of_speech(self):
-        pass
 
 
 class TestParseNotFound:
     def setup_method(self):
-        path = get_path("brah")
-        self.parsed_data = get_text(path)
+        self.word_name = "notfound"
+        path = get_path(self.word_name)
+        self.html = get_html(path)
+        self.parsed = Parse.parse(self.word_name, self.html)
+
+    def test_has_no_definitions(self):
+        assert self.parsed[self.word_name] == "not found"
+
+    def test_get_part_of_speech(self):
+        assert self.parsed["type_of_speech"] == []
+
+
+@pytest.mark.skip()
+class TestParseNoun:
+    def setup_method(self):
+        path = get_path("dictionary")
+        self.parsed_data = get_html(path)
 
     def test_get_part_of_speech(self):
         pass
 
 
+@pytest.mark.skip()
+class TestParseVerb:
+    def setup_method(self):
+        path = get_path("learn")
+        self.parsed_data = get_html(path)
+
+    def test_get_part_of_speech(self):
+        pass
+
+
+@pytest.mark.skip()
+class TestParseAdjective:
+    def setup_method(self):
+        path = get_path("tremendous")
+        self.parsed_data = get_html(path)
+
+    def test_get_part_of_speech(self):
+        pass
+
+
+@pytest.mark.skip()
+class TestParsePreposition:
+    def setup_method(self):
+        self.word_name = "into"
+        path = get_path(self.word_name)
+        self.html = get_html(path)
+        self.parsed = Parse.parse(self.word_name, self.html)
+
+    def test_get_part_of_speech(self):
+        pass
+
+
+@pytest.mark.skip()
+class TestParseConjunction:
+    def setup_method(self):
+        path = get_path("although")
+        self.parsed_data = get_html(path)
+
+    def test_get_part_of_speech(self):
+        pass
+
+
+@pytest.mark.skip()
 class TestParseMultiplePartOfSpeech:
     def setup_method(self):
         path = get_path("take")
-        self.parsed_data = get_text(path)
+        self.parsed_data = get_html(path)
 
     def test_get_part_of_speech(self):
         pass
