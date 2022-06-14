dd970a998eca5fa4a7f0e1ffb61d0f95f6baaf78
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2022-06-14 20:57:04 +0900

    test: skip TestFetchWord test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,12 @@
 # tests/test_api.py
+import pytest
+
 from src.api import fetch_word
 
 
+@pytest.mark.skipif(
+    True, reason="when this test runs, this always gets HUGE html from Weblio."
+)
 class TestFetchWord:
     def test_fetch_word_is_success(self):
         # when fetch_data == 404 tells us that weblio site is down
